6d55108bbc13d4315bb0e8f11a6f8efa4d7f619e
Yannic Hock <yaho00001@stud.uni-saarland.de>
2023-11-28 21:36:30 +0100

    WIP

--- pages/api/getEnv.ts
+++ pages/api/getEnv.ts
@@ -20,13 +20,17 @@ export default async function handler(
   const PORTAINER_PORT = process.env.PORTAINER_PORT;
   const BASE_URL = process.env.BASE_URL;
   const EDITOR_PORT = process.env.EDITOR_PORT;
+  const PACMAN_PORT = process.env.PACMAN_PORT;
+  const BACKEND_PORT = process.env.BACKEND_PORT;
 
 
-  if(PORTAINER_USERNAME && PORTAINER_PASSWORD && PORTAINER_PORT && BASE_URL && EDITOR_PORT){
+  if(PORTAINER_USERNAME && PORTAINER_PASSWORD && PORTAINER_PORT && BASE_URL && EDITOR_PORT && PACMAN_PORT && BACKEND_PORT){
     res.status(200).send({
       PORTAINER_PORT: PORTAINER_PORT,
       BASE_URL: BASE_URL,
-      EDITOR_PORT: EDITOR_PORT
+      EDITOR_PORT: EDITOR_PORT,
+      PACMAN_PORT: PACMAN_PORT,
+      BACKEND_PORT: BACKEND_PORT
     });
   } else {
     res.status(500).send("Missing Environment Variables")
