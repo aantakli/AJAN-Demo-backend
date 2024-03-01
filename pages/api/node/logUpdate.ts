b270a7e4d8d66ddcbbc8ebcfc00b734b97e25b03
Yannic Hock <yannic.hock@dfki.de>
2024-03-01 11:01:14 +0100

    WIP Live Log

--- pages/api/node/logUpdate.ts
+++ pages/api/node/logUpdate.ts
@@ -16,10 +16,9 @@ export default async function handler(
 
     if(PORTAINER_USERNAME && PORTAINER_PASSWORD && PORTAINER_PORT && BASE_URL){
         const portainer = new PortainerClient(`${BASE_URL}:${PORTAINER_PORT}`, PORTAINER_USERNAME, PORTAINER_PASSWORD);
-        console.log(id)
         let containerData = await portainer.callApiWithKey('get', '/api/endpoints/2/docker/containers/' + id + '/logs?since=0&stderr=1&stdout=1&tail=100&timestamps=0')
-        console.log(containerData)
-        res.status(200).send('')
+        res.status(200).send(containerData)
+        return
     }
     res.status(400).send("Error in Environment Variables")
 }
