a2f50778cc88aad9094d8163998492a3c7ff2e9e
Yannic Hock <yannic.hock@dfki.de>
2024-03-01 13:11:09 +0100

    WIP Live Log

--- pages/index.tsx
+++ pages/index.tsx
@@ -94,7 +94,7 @@ useEffect(() => {
         setLog(res.data);
       } else {
         let diff = findRestofString(res.data, log)
-        setLog(res.data.replaceAll("", "").replaceAll("�", ""))
+        setLog(res.data.replaceAll("", "").replaceAll("�", "").replaceAll("\t", ""))
       }
     })
   }
